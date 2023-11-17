f5451f6963fb1a6b66a8fa281e24823e78c5181a
MichaelMantovani <michmantovani@gmail.com>
2023-11-17 17:20:49 +0100

    add char frequency count and test it

--- src/org/lessons/java/charscounter/Main.java
+++ src/org/lessons/java/charscounter/Main.java
@@ -1,7 +1,25 @@
 package org.lessons.java.charscounter;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 public class Main {
 	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
 
+		System.out.println("Scrivi una parola");
+		String word = in.nextLine();
+		in.close();
+		Map<Character, Integer> charFrequency = new HashMap<>();
+
+		for (Character c : word.toCharArray()) {
+			if (charFrequency.containsKey(c))
+				charFrequency.put(c, charFrequency.get(c) + 1);
+			else
+				charFrequency.put(c, 1);
+		}
+		
+		System.out.println(charFrequency);
 	}
 }
